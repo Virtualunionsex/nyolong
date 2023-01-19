324dc132c16aa6ba49cf260e1905565eb43c920b
Shinigamibots5 <100681700+Shinigamibots5@users.noreply.github.com>
2023-01-19 16:21:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,17 @@ import logging
 class Config:
   
 #Your API HASH from my.telegram.org    
-    API_ID = int(os.environ.get("API_ID", "1945966"))
+    API_ID = int(os.environ.get("API_ID", "18576653"))
 #Your API HASH from my.telegram.org      
-    API_HASH = os.environ.get("API_HASH", "6b73197f50512e26f5ebd42e73c3315f")
+    API_HASH = os.environ.get("API_HASH", "d29fa01d174ec2ac0d5bd415f052d173")
 #BOT TOKEN: @Botfather on telegram    
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5740014121:AAFEF_Jpj0k_rzljg4oImLrdH9BCLgRl_D4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5647123835:AAHN6PUFsVpCFkxYuSdHlX4iW_Wc8Nuo7IU")
 #Public channel username without '@'. Don't forget to add bot in channel as administrator.    
-    FORCESUB = os.environ.get("FORCESUB", "after_ever_happy_end") 
+    FORCESUB = os.environ.get("FORCESUB", "akimaxmovies") 
 #Owner user id   
     AUTH  = os.environ.get("OWNER_ID", "5143506371")
 #Pyrogram string session using (https://replit.com/@KindKobra/Pyrogram-String-Gen?v=1)
-    SESSION = os.environ.get("SESSION", "AQA5aXqC5nSWIQHtNn0wDfBO1fz6pyxNHTEh2AHVSYPB_SzoEO0miF_4naTMruoVM3fnmX_QwmSSR6UK_jr-au4Z923JEp-6qlYDOz0m6vDzwTzX3paHRVAiOg5yBFDII9MlDeLKIAg-CJPttolByprAPL3RBej7YPL5Ap3iJztKIqNNBPD4sjmWKwMfFDhUikFr_Bb4iqLMb5cESSb6eY0c9xRIulIRmVKYvFJjD5-cODh9jvhXKRWxC-afvua9kCZDsQpuf8W-emwUQTSHgZq4MRwPTHdsHJio08d9bmZ_quxB8VYekdug-1yBP6vjppCfiZnw8y8celMewDvBWzR5AAAAATKTrcMA")
+    SESSION = os.environ.get("SESSION", "AQCV8ShRKtiHlOomLaPBDfs2MQm6y9vnVbBwNtkqTp-fZj8RTw3nCbwsk9tw-V6KGztX9sfj8uxAwb0OpvEx__CBjO0DGnSuNv05xYbTikMrqe_HuPyhsCYVsEw9F32KKwKAxKE8Uf7nhAUvwMOqRqCmiXc44Z6BiuaYc91zNm6AeHox_-YzeXF6frc-bfPGadZt9iwWOvNfS4Rdyo21_SmpkDH7iZ7ydVtk_kLn4c-pHtK1iwU_HNW2VXskeNVv2wYSyhQXMwQRvY-jTusB5z2etzvAr4Vhm4H4I4jAnjxABaDeWULzW9Z5nAfbvyxIEfbwJyEa2pE8HDNURM_ZkmzqAAAAATKTrcMA")
   
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
