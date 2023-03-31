1e55effb9c37c4034aca78b489fea1cbcc3297de
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-03-31 07:09:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,17 @@ import logging
 class Config:
   
 #Your API HASH from my.telegram.org    
-    API_ID = int(os.environ.get("API_ID", "18576653"))
+    API_ID = int(os.environ.get("API_ID", "16246834"))
 #Your API HASH from my.telegram.org      
-    API_HASH = os.environ.get("API_HASH", "d29fa01d174ec2ac0d5bd415f052d173")
+    API_HASH = os.environ.get("API_HASH", "29b3ffa9245c07f05375b92f38e8f13d")
 #BOT TOKEN: @Botfather on telegram    
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5647123835:AAHN6PUFsVpCFkxYuSdHlX4iW_Wc8Nuo7IU")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5716827359:AAE3NJPyZkXHATL8m3AmLVxFy0-NNhKOOog")
 #Public channel username without '@'. Don't forget to add bot in channel as administrator.    
-    FORCESUB = os.environ.get("FORCESUB", "akimax") 
+    FORCESUB = os.environ.get("FORCESUB", "nakama_asl") 
 #Owner user id   
-    AUTH  = os.environ.get("OWNER_ID", "5143506371")
+    AUTH  = os.environ.get("OWNER_ID", "1715348447")
 #Pyrogram string session using (https://replit.com/@KindKobra/Pyrogram-String-Gen?v=1)
-    SESSION = os.environ.get("SESSION", "AQCV8ShRKtiHlOomLaPBDfs2MQm6y9vnVbBwNtkqTp-fZj8RTw3nCbwsk9tw-V6KGztX9sfj8uxAwb0OpvEx__CBjO0DGnSuNv05xYbTikMrqe_HuPyhsCYVsEw9F32KKwKAxKE8Uf7nhAUvwMOqRqCmiXc44Z6BiuaYc91zNm6AeHox_-YzeXF6frc-bfPGadZt9iwWOvNfS4Rdyo21_SmpkDH7iZ7ydVtk_kLn4c-pHtK1iwU_HNW2VXskeNVv2wYSyhQXMwQRvY-jTusB5z2etzvAr4Vhm4H4I4jAnjxABaDeWULzW9Z5nAfbvyxIEfbwJyEa2pE8HDNURM_ZkmzqAAAAATKTrcMA")
+    SESSION = os.environ.get("SESSION", "AQA2XUL_FSP0tZDev1APSt4yiPX5z8wHwwV_Eo8PEp1u2PzVzzZbz-Weaw4f8ivwCvHD0_tVI51tGVENPzAv5JkjhjonCvsWoPQi5Af3arDeDc_jqGv0ct_8mqBYQAmyklCv9LorAO_kKI6yWT05zyrjkcxodW8RPMlcuMmWDbdDHJfGT3EodeIq5I4kcCjytTubRGTBkIAdOv8YuXmrzENG33yNv0NbmDtXmKY_98D5rXeSQjvcBUfHREw7fUbQ0g2YH5V9hYHrlpehTymXaLtQIa03kFan-oKjqrJgHPeh47dWXE3p1y5389yz-0ldEYOXX-oHBO8NQUv6f0jE8QILZj4j3wA")
   
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
